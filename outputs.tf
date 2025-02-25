1a1cf286379cba67613200f8f04b3f19afb08f6f
zmf <1950417697@qq.com>
2025-02-25 15:42:55 +0800

    Create outputs.tf
    1

---
+++ outputs.tf
@@ -0,0 +1,99 @@
+output "private_ipv4" {
+  description = "private_ipv4"
+  value       = ctcloud_b_ecs.b_ecs.private_ipv4
+}
+
+output "private_ipv6" {
+  description = "private_ipv6"
+  value       = ctcloud_b_ecs.b_ecs.private_ipv6
+}
+
+output "resource_id" {
+  description = "resource_id"
+  value       = ctcloud_b_ecs.b_ecs.resource_id
+}
+
+output "os_type" {
+  description = "os_type"
+  value       = ctcloud_b_ecs.b_ecs.os_type
+}
+
+output "instance_status" {
+  description = "instance_status"
+  value       = ctcloud_b_ecs.b_ecs.instance_status
+}
+
+output "create_time" {
+  description = "create_time"
+  value       = ctcloud_b_ecs.b_ecs.create_time
+}
+
+output "update_time" {
+  description = "update_time"
+  value       = ctcloud_b_ecs.b_ecs.update_time
+}
+
+output "available_day" {
+  description = "available_day"
+  value       = ctcloud_b_ecs.b_ecs.available_day
+}
+
+output "image_name" {
+  description = "image_name"
+  value       = ctcloud_b_ecs.b_ecs.image_name
+}
+
+output "vpc_name" {
+  description = "vpc_name"
+  value       = ctcloud_b_ecs.b_ecs.vpc_name
+}
+
+output "floating_ip" {
+  description = "floating_ip"
+  value       = ctcloud_b_ecs.b_ecs.floating_ip
+}
+
+output "key_pair_name" {
+  description = "key_pair_name"
+  value       = ctcloud_b_ecs.b_ecs.key_pair_name
+}
+
+output "deletion_protection" {
+  description = "deletion_protection"
+  value       = ctcloud_b_ecs.b_ecs.deletion_protection
+}
+
+output "security_group_ids" {
+  description = "security_group_ids"
+  value       = ctcloud_b_ecs.b_ecs.security_group_ids
+}
+
+output "expire_time" {
+  description = "expire_time"
+  value       = ctcloud_b_ecs.b_ecs.expire_time
+}
+
+output "instance_id" {
+  description = "status"
+  value       = ctcloud_b_ecs.b_ecs.instance_id
+}
+
+output "network_card_list_out" {
+  description = "network_card_list_out"
+  value       = ctcloud_b_ecs.b_ecs.network_card_list_out
+}
+
+output "display_name" {
+  description = "display_name"
+  value       = ctcloud_b_ecs.b_ecs.display_name
+}
+
+output "instance_name" {
+  description = "instance_name"
+  value       = ctcloud_b_ecs.b_ecs.instance_name
+}
+
+output "data_disk_list" {
+  description = "data_disk_list"
+  value       = ctcloud_b_ecs.b_ecs.data_disk_list
+}
